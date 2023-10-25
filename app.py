bfa967b41d28afea46315ce72a670904c81893af
Stephen-Sim <simshansiong2002@gmail.com>
2023-10-25 23:11:27 +0800

    fix bug

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
-from keras.preprocessing import image
+from keras.preprocessing.image import load_img, img_to_array
 
 import numpy as np
 import base64
@@ -17,8 +17,8 @@ def is_cat(image_data):
         image_io = io.BytesIO(image_data)
 
         # Load and process the image
-        img = image.load_img(image_io, target_size=(224, 224))
-        x = image.img_to_array(img)
+        img = load_img(image_io, target_size=(224, 224))
+        x = img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
 
